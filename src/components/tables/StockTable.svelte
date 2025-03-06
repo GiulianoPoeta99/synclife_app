b060cc10aaa8d7c315e93b9f0e78bad98a76bca0
LucasA21 <lucasaraya19@hotmail.com>
2025-03-06 20:32:27 -0300

    feat: :zap: Se arranco diseño stock

---
+++ src/components/tables/StockTable.svelte
@@ -0,0 +1,59 @@
+<script></script>
+
+<style></style>
+
+<div class="overflow-x-auto">
+    <table class="table w-full border-separate border-spacing-y-2 max-w-[1300px]">
+      <!-- head -->
+      <thead class="text-black text-left">
+        <tr>
+          <th class="px-4 py-3">Type</th>
+          <th class="px-4 py-3">Name</th>
+          <th class="px-4 py-3">Amount</th>
+          <th class="px-4 py-3">Maturity</th>
+          <th class="px-4 py-3">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- row 1 -->
+        <tr class="bg-[rgba(213,147,209,0.28)] text-black rounded-full overflow-hidden">
+          <td class="px-4 py-3 rounded-l-full">dairy</td>
+          <td class="px-4 py-3">Milk</td>
+          <td class="px-4 py-3">5</td>
+          <td class="px-4 py-3">2 months</td>
+          <td class="px-4 py-3 flex gap-2 rounded-r-full">
+            <button class="p-2 bg-white rounded-full shadow-md">➕</button>
+            <button class="p-2 bg-white rounded-full shadow-md">✏️</button>
+            <button class="p-2 bg-white rounded-full shadow-md">🗑️</button>
+          </td>
+        </tr>
+
+        <!-- row 2 -->
+        <tr class="bg-[rgba(213,147,209,0.60)] text-black rounded-full overflow-hidden">
+          <td class="px-4 py-3 rounded-l-full">dairy</td>
+          <td class="px-4 py-3">Butter</td>
+          <td class="px-4 py-3">2</td>
+          <td class="px-4 py-3">10 days</td>
+          <td class="px-4 py-3 flex gap-2 rounded-r-full">
+            <button class="p-2 bg-white rounded-full shadow-md">➕</button>
+            <button class="p-2 bg-white rounded-full shadow-md">✏️</button>
+            <button class="p-2 bg-white rounded-full shadow-md">🗑️</button>
+          </td>
+        </tr>
+
+        <!-- row 3 -->
+        <tr class="bg-[rgba(213,147,209,0.28)] text-black rounded-full overflow-hidden">
+          <td class="px-4 py-3 rounded-l-full">vegetable</td>
+          <td class="px-4 py-3">Tomato</td>
+          <td class="px-4 py-3">6</td>
+          <td class="px-4 py-3">7 days</td>
+          <td class="px-4 py-3 flex gap-2 rounded-r-full">
+            <button class="p-2 bg-white rounded-full shadow-md">➕</button>
+            <button class="p-2 bg-white rounded-full shadow-md">✏️</button>
+            <button class="p-2 bg-white rounded-full shadow-md">🗑️</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+</div>
+
